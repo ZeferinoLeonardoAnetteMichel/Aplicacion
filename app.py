afd844ec01e36552f47f74e75bac8642ee778e3b
ZeferinoLeonardoAnetteMichel <23308060610457@cetis61.edu.mx>
2025-11-05 17:19:38 -0700

    cambios

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 
+USUARIOS_REGISTRADO:[]
+
 app = Flask(__name__)
 @app.route('/')
 def inicio():
@@ -17,5 +19,14 @@ def descubrete():
 def recetas():
     return render_template('recetas.html')
 
+@app.route('/videos')
+def videos():
+    return render_template('videos.html')
+
+#Hay que poner las alergias y eso debe ser guardada en base de datos, para el lunes en un diccionario de datos,articulos con etiquetas
+#Buscar una o dos dietas de moda y poner mitos y verdades, guia sobre macronutrientes, importancia de la hidratacion y fibra
+#un plan d ejercicio descargable 1 o dos y gratuitos
+#Banco de recetas saludables
+#Diferentes tipos de calculadoras corporal,basal,gasto calorico, en base a los datos ingresados acceso a todos
 if __name__ == '__main__':
     app.run(debug=True)
