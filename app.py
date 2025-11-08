2958692ed4dce08cc20f6373e2f0af21fa3cff5b
ZeferinoLeonardoAnetteMichel <23308060610457@cetis61.edu.mx>
2025-11-08 12:28:17 -0700

    cambios

--- app.py
+++ app.py
@@ -1,14 +1,70 @@
-from flask import Flask, render_template, url_for, request, redirect
-
-USUARIOS_REGISTRADO:[]
+from flask import Flask, render_template,request, redirect, url_for, flash
 
 app = Flask(__name__)
+
+USERS = {
+    'nombre' : 'anette',
+    'apellido' : 'leonardo',
+    'email' : 'anette@gmail',
+    'password' :'anet123'
+},{
+    'nombre' : 'jazmin',
+    'apellido' :'regis',
+    'email' : 'jaz@gmail.com',
+    'password' : 'jaz123'
+}
+
 @app.route('/')
 def inicio():
     return render_template('inicio.html')
 
 @app.route('/registro')
 def registro():
+    error = None
+    if request.method == "POST":
+        nombre = request.form.get('nombre') 
+        apellido = request.form.get('apellido')
+        email = request.form.get('email') 
+        peso = request.form.get('peso')
+        estatura = request.form.get('estatura')
+        edad = request.form.get('edad')
+        preferencia = request.form.get('preferencia')
+        sexo = request.form.get('sexo')
+        fisica = request.form.get('fisica')
+        objetivo = request.form.get('objetivo')
+        cocina = request.form.get('cocina')
+        password = request.form.get('password') 
+        confirmPassword = request.form.get('confirmaContraseña') 
+        if not all([nombre, apellido, email, peso, estatura, edad, preferencia, sexo, fisica, objetivo, cocina, password, confirmPassword]):
+            error = 'Todos los campos son obligatorios.'
+        if error is None:
+                    error = f'El correo electrónico "{email}" ya está registrado.'
+        if error is None and password != confirmPassword:
+            error = "La contraseña no coincide" 
+        if error is not None:
+            flash(error, 'danger')
+            return redirect(url_for('inicio.html')) 
+        else:
+            new_user = {
+                'nombre': nombre,
+                'apellido': apellido,
+                'email': email,
+                'peso': peso,
+                'estatura' : estatura,
+                'edad' : edad,
+                'preferecia' : preferencia,
+                'sexo' : sexo,
+                'fisica' : fisica,
+                'objetivo' : objetivo,
+                'cocina' : cocina,
+                'password': password 
+                
+            }
+            flash('¡Registro exitoso! Ahora puedes iniciar sesión.', 'success')
+            return redirect(url_for('login'))
+
+    return render_template('registro.html', no_menu=True)
+
     return render_template('registro.html')
 
 @app.route('/descubrete')
